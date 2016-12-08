c19607241b7c2ee7f04955de86b2059b8463670f
bourgesl <bourges.laurent@gmail.com>
2016-12-08 23:28:51 +0100

    added tests

--- src/main/java/test/TestNonAARasterization.java
+++ src/main/java/test/TestNonAARasterization.java
@@ -6,8 +6,6 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.PathIterator;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -59,7 +57,7 @@ public class TestNonAARasterization extends Application {
     static final int NUMTESTS = 100000;
     static final int TESTW = 50;
     static final int TESTH = 50;
-    static final int MAG = 15;
+    static final int MAG = 18;
 
     static final int BGPIXEL = 0xffffffff;
     static final int FGPIXEL = 0xff000000;
@@ -97,7 +95,7 @@ public class TestNonAARasterization extends Application {
         }
     }
 
-    List<Result> badpaths = new ArrayList<>();
+    int numBadPaths = 0;
     Result worst;
     int numpathstested;
     long totalerrors;
@@ -120,7 +118,7 @@ public class TestNonAARasterization extends Application {
         resultpath.setSmooth(false);
         resultpath.getTransforms().add(new Scale(MAG, MAG));
         resultpath.setFill(null);
-        resultpath.setStroke(Color.BLACK);
+        resultpath.setStroke(Color.DARKGRAY);
         resultpath.setStrokeWidth(1.0 / MAG);
         root.setCenter(new Group(resultcv, resultpath));
 
@@ -289,7 +287,7 @@ public class TestNonAARasterization extends Application {
     }
 
     public void updateLabel() {
-        int numbadpaths = badpaths.size();
+        int numbadpaths = numBadPaths;
         double percentbadpaths = numbadpaths * 100.0 / numpathstested;
         double avgbadpixels = (totalerrors == 0) ? 0.0 : totalerrors * 1.0 / numbadpaths;
         double avgwarnings = (totalwarnings == 0) ? 0.0 : totalwarnings * 1.0 / numbadpaths;
@@ -350,7 +348,7 @@ public class TestNonAARasterization extends Application {
     }
 
     public void addResult(Result r, int numtested) {
-        badpaths.add(r);
+        numBadPaths++;
         if (r.worseThan(worst)) {
             worst = r;
         }
